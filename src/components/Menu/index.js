92848e96eadaaa1611af980200b1ae82025d3264
Lucas de Melo Lima <lucas.melo-ext@viavarejo.com.br>
2021-02-14 22:27:48 -0300

    Ajustes gerais

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import "./styles.css";
 import personal from "../../pdf/personal.jpg";
 
-
 const Menu = () => {
   return (
-    <div className="menu-container">
+    <header className="menu-container">
       <h1>Flavio Riva</h1>
       <p>Doctoral Candidate in Public Administration and Government, Fundação Getulio Vargas (FGV) </p>
       <img src={personal} alt="Personal" />
@@ -17,7 +16,7 @@ const Menu = () => {
         <a href="./#learning">Learning</a>
         <a href="./#publications">Publications</a>
       </div>
-    </div>
+    </header>
   );
 };
 
